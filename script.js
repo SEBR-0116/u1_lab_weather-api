db1ca5e00b58415b6f967f1e8b920090bacebe7a
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-01-25 14:23:08 -0500

    Weather app using Weather API

--- script.js
+++ script.js
@@ -10,23 +10,28 @@ let cityName = document.querySelector('#cityName');
 let cityTempInF = document.querySelector('#tempInF');
 let cityTempInC = document.querySelector('#tempInC');
 let humidityInCity = document.querySelector('#humidity');
+let countryLocation = document.querySelector('#locationCountry');
 
 button.addEventListener('click', async () => {
     let city = input.value;
+    
     let response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`);
+    let cityCapName = response.data.location.name;
     let tempInF = response.data.current.temp_f;
     let tempInC = response.data.current.temp_c;
     let cityWeatherIcon = response.data.current.condition.icon;
     let cityHumidity = response.data.current.humidity;
+    let cityCountry = response.data.location.country;
     console.log(city);
     console.log(`Temperature in Celcius: ${tempInC}`);
     console.log(response);
     
     //Output
-    cityName.innerHTML = `${city}`;
+    cityName.innerHTML = `${cityCapName}`;
     cityTempInF.innerHTML = `${tempInF}&deg; Fahrenheit`;
     cityTempInC.innerHTML = `${tempInC}&deg; Celcius`;
     humidityInCity.innerHTML = `${cityHumidity}&#37;`;
+    countryLocation.innerHTML = `${cityCountry}`;
     weatherIcon.setAttribute('src', `${cityWeatherIcon}`);
     weatherCard.style.opacity ='1';
 });
